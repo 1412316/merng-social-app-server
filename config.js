fe5b385120dbb9c68bd2d9abdc143dd878ba7214
NhatMinh <nhatminhdang1996@gmail.com>
2021-09-08 11:46:12 +0700

    add config

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB: 'mongodb+srv://1412316:darkcyber1260@cluster0.y9bb6.mongodb.net/MERNG-social-media?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+}
